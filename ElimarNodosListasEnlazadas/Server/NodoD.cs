cf6c179d6d960d0c09f56cdb227278aea2af4c5d
Nelson Barahona <nelsonluisbarahona@gmail.com>
2025-04-04 11:54:24 -0600

    Cmbios agregado

---
+++ ElimarNodosListasEnlazadas/Server/NodoD.cs
@@ -0,0 +1,29 @@
+﻿namespace ElimarNodosListasEnlazadas.Server
+{
+    public class NodoD
+    {
+        public string Informacion { get; set; }
+        public NodoD? ReferenciaSiguiente { get; set; }
+        public NodoD? ReferenciaAnterior { get; set; }
+
+
+        public NodoD()
+        {
+            Informacion = string.Empty;
+            ReferenciaSiguiente = null;
+            ReferenciaAnterior = null;
+        }
+
+        public NodoD(string informacion)
+        {
+            Informacion = informacion;
+            ReferenciaSiguiente = null;
+            ReferenciaAnterior = null;
+        }
+
+        public override string ToString()
+        {
+            return $"{Informacion}";
+        }
+    }
+}
